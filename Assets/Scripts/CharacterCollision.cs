f1aa5c2b78ca72beb6d65259eb985cbdb7b0d57c
YUEWEI <shelter101@qq.com>
2022-11-16 19:09:57 +0900

    11.16

--- Assets/Scripts/CharacterCollision.cs
+++ Assets/Scripts/CharacterCollision.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class CharacterCollision : MonoBehaviour
 {
     public Transform scene;
+    //public float pushPower = 1.0f;
+    public float forceMagnitude = 1.0f;
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
@@ -17,5 +20,22 @@ public class CharacterCollision : MonoBehaviour
         //{
         //    transform.SetParent(scene);
         //}
+
+        if (hit.gameObject.CompareTag("Seesaw"))
+        {
+            Rigidbody rb = hit.collider.attachedRigidbody;
+
+            if (rb == null || rb.isKinematic)
+            {
+                return;
+            }
+
+            //if (hit.moveDirection.y > -0.3f)
+            //{
+            //    return;
+            //}
+
+            rb.AddForceAtPosition(Vector3.down * forceMagnitude, transform.position, ForceMode.Impulse);
+        }
     }
 }
