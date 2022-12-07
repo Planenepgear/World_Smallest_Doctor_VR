1b0461522b749467ebae7b72ebbb732c6c772ed2
YUEWEI <shelter101@qq.com>
2022-12-07 19:56:48 +0900

    functional 2 (please use the next one)

--- Assets/Scripts/HandCheck.cs
+++ Assets/Scripts/HandCheck.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class HandCheck : MonoBehaviour
 {
+    public GameObject hand;
+    public GameObject handModel;
+    public float minDistance = 0.1f;
+
+    public bool isLeftHand;
+    public bool isRightHand;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        handModel.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.transform.CompareTag("Hands"))
+        if (hand && isLeftHand)
         {
-
+            if((transform.position - hand.transform.position).sqrMagnitude <= minDistance)
+            {
+                hand.SetActive(false);
+                handModel.SetActive(true);
+                //transform.parent.SendMessage("",)
+            }
+        }
+        else if (hand && isRightHand)
+        {
+            if ((transform.position - hand.transform.position).sqrMagnitude <= minDistance)
+            {
+                hand.SetActive(false);
+                handModel.SetActive(true);
+                //transform.parent.SendMessage("",)
+            }
         }
     }
 }
